b3181124c48684e20931f9f897d3af1fab66cb50
AlishaAmber <161523298+AlishaAmber@users.noreply.github.com>
2025-05-01 15:12:14 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -396,7 +396,7 @@ elif st.session_state.current_view =="stats":
         with col2:
             st.metric("Book Read", stats['read_books'])
         with col3:
-        st.metric("Percentage Read", f"{stats['percentage_read']:.1f}%")
+            st.metric("Percentage Read", f"{stats['percentage_read']:.1f}%")
         create_visualization()
 
         if stats['authors']:
